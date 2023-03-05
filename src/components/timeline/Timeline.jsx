0861effa80210fff9e221aa53b74e3224bbc81f9
Aarjav Jain <aarjavjain2309@gmail.com>
2023-03-05 17:58:27 +0530

    improved code formatiing and minor changes in css

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,36 +1,40 @@
-import { VerticalTimeline }  from 'react-vertical-timeline-component';
-import TimelineElement from './Timelineprops';
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import TimelineElement from "./Timelineprops";
 
-function Timeline(){
-    return(
-      <div>
-      <span className='TimelineSpan' ><h3 className='TimelineHeading'>Timeline</h3></span>
+function Timeline() {
+  return (
+    <div>
+    <h3 className="TimelineSpan">Timeline</h3>
 
-<VerticalTimeline>
-  
-<TimelineElement  
-heading1 = "Art Director"
-heading2 = "Miami, FL"
-content  = "Creative Direction, User Experience, Visual Design, Project Management, Team Leading"/>
-<TimelineElement  
-heading1 = "Art Director"
-heading2 = "Miami, FL"
-content  = "Creative Direction, User Experience, Visual Design, Project Management, Team Leading"/>
-<TimelineElement  
-heading1 = "Art Director"
-heading2 = "Miami, FL"
-content  = "Creative Direction, User Experience, Visual Design, Project Management, Team Leading"/>
-<TimelineElement  
-heading1 = "Art Director"
-heading2 = "Miami, FL"
-content  = "Creative Direction, User Experience, Visual Design, Project Management, Team Leading"/>
-<TimelineElement  
-heading1 = "Art Director"
-heading2 = "Miami, FL"
-content  = "Creative Direction, User Experience, Visual Design, Project Management, Team Leading"/>
-</VerticalTimeline>
-</div>
-    )
+      <VerticalTimeline>
+        <TimelineElement
+          heading1="Art Director"
+          heading2="Miami, FL"
+          content="Creative Direction, User Experience, Visual Design, Project Management, Team Leading"
+        />
+        <TimelineElement
+          heading1="Art Director"
+          heading2="Miami, FL"
+          content="Creative Direction, User Experience, Visual Design, Project Management, Team Leading"
+        />
+        <TimelineElement
+          heading1="Art Director"
+          heading2="Miami, FL"
+          content="Creative Direction, User Experience, Visual Design, Project Management, Team Leading"
+        />
+        <TimelineElement
+          heading1="Art Director"
+          heading2="Miami, FL"
+          content="Creative Direction, User Experience, Visual Design, Project Management, Team Leading"
+        />
+        <TimelineElement
+          heading1="Art Director"
+          heading2="Miami, FL"
+          content="Creative Direction, User Experience, Visual Design, Project Management, Team Leading"
+        />
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Timeline;
