c7109060a88f63d8c5f81fe84de037f34a60ecfc
mdathar <mdathar4403@gmail.com>
2023-03-09 00:40:21 +0530

    TeamPage

---
+++ src/pages/TeamPage/GalleryData.js
@@ -0,0 +1,75 @@
+export const GalleryData = [
+    {
+        id:1,
+        title:"car",
+        name:"John",
+        image:"./images/a1.jpg"
+    },
+    {
+        id:3,
+        title:"car",
+        name:"Riya",
+        image:"./images/a2.jpg"
+    },
+    {
+        id:2,
+        title:"car",
+        name:"Sonu",
+        image:"./images/a3.jpg"
+    },
+    {
+        id:4,
+        title:"car",
+        name: "Rahul",
+        image:"./images/a4.jpg"
+    },
+    {
+        id:5,
+        title:"bike",
+        name: "Rocky",
+        image:"./images/b1.jpg"
+    },
+    {
+        id:7,
+        title:"bike",
+        name: "Jack",
+        image:"./images/b2.jpg"
+    },
+    {
+        id:7,
+        title:"car",
+        name:"John",
+        image:"./images/a1.jpg"
+    },
+    {
+        id:8,
+        title:"car",
+        name:"Riya",
+        image:"./images/a2.jpg"
+    },
+    {
+        id:9,
+        title:"car2",
+        name:"Sonu",
+        image:"./images/a3.jpg"
+    },
+    {
+        id:10,
+        title:"car2",
+        name: "Rahul",
+        image:"./images/a4.jpg"
+    },
+    {
+        id:11,
+        title:"bike2",
+        name: "Rocky",
+        image:"./images/b1.jpg"
+    },
+    {
+        id:12,
+        title:"bike2",
+        name: "Jack",
+        image:"./images/b2.jpg"
+    },
+    
+]
--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,13 +1,74 @@
-import React, { useState } from 'react'
-import Dropdown from './Dropdown/Dropdown'
+import { GalleryData } from "./GalleryData";
+import { useEffect, useState } from "react";
+// import car from './images/'
+import './TeamPage.css'
+import { motion, AnimatePresence } from "framer-motion";
 
-const TeamPage = () => {
-  const[selected, setSelected]= useState("Convenors");
+function TeamPage() {
+
+  const [data, setData] = useState([]);
+  const [collection, setCollection] = useState([]);
+  const [active, setActive ] = useState('');
+  const [activebtn, setActiveBtn ] = useState('');
+
+  useEffect(() => {
+    setData(GalleryData);
+    setCollection([...new Set(GalleryData.map((item) => item.title))])
+  }, [])
+
+  const gallery_filter = (itemData) => {
+    const filterData = GalleryData.filter((item) => item.title === itemData);
+    console.log(itemData)
+    setData(filterData);
+    setActive(itemData);
+    setActiveBtn('')
+  }
+  function setColor(){
+    setData(GalleryData)
+    setActiveBtn('#')
+    setActive("")
+  }
+
   return (
     <div>
-      <Dropdown selected={selected} setSelected={setSelected} />
-    </div>
-  )
+      <div className="galleryWrapper">
+        <div className="filterItem">
+          <ul>
+            <li>
+              <button onClick={setColor}
+              className={activebtn === '#' ? 'selected':" "}>All</button>
+            </li>
+            {
+              collection.map((item) =>
+                <li>
+                  <button onClick={() => { gallery_filter(item) }} 
+                    className={active === item ? 'selected':" "}>{item}</button>
+                </li>)}
+          </ul>
+        </div>
+        <motion.div
+          layout
+          className="galleryContainer">
+          <AnimatePresence>
+            {
+              data.map((item) =>
+                <motion.div
+                  animate={{ opacity: 1, scale: 1 }}
+                  initial={{ opacity: 0, scale: 0 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  transition={{ duration: 0.36 }}
+                  layout
+                  key={item.id} 
+                  className="galleryItem">
+                    <img src={item.image} alt="here" />
+                    <p className="position-title">{item.name}</p>
+                </motion.div>
+              )}
+          </AnimatePresence>
+        </motion.div>
+      </div>
+    </div >
+  );
 }
 
 export default TeamPage
