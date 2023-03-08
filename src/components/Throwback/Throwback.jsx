ce280d227fed69ec024234a561876754aca4d4b6
Aarjav Jain <aarjavjain2309@gmail.com>
2023-03-08 22:59:00 +0530

    solved css issues

--- src/components/Throwback/Throwback.jsx
+++ src/components/Throwback/Throwback.jsx
@@ -1,37 +1,42 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-import "./Throwback.css"
-import Group1 from"../../assets/33630.png"
-import Group2 from"../../assets/33631.png"
+import "./Throwback.css";
+import Group1 from "../../assets/33630.png";
+import Group2 from "../../assets/33631.png";
 function Throwback() {
   return (
     <div>
-      
-     <div className="throwback">
-
-    <div className="head">
-      <p>Throwback</p>
+      <div className="throwback">
+        <div className="head">
+          <p>Throwback</p>
+        </div>
+        <div className="throwback-wrapper">
+          <div className="img1">
+            <img className="img1size" src={Group2} alt="op" />
+          </div>
+          <div className="right-wrapper">
+            <div className="content">
+              Here's the glimpse of the previous editions of Jagriti. Exciting
+              events ranging from guest talks to competitions were successfully
+              held,inundated with participants from all over India.
+            </div>
+            <div className="img2">
+              <img className="img2size" src={Group1} alt="op" />
+            </div>
+          </div>
+          {/* <div className="allimg">
+            <div className="img1">
+              <img className="img1size" src={Group2} alt="op" />
+            </div>
+            <div className="img2">
+              <img className="img2size" src={Group1} alt="op" />
+            </div>
+          </div> */}
+        </div>
+      </div>
     </div>
-    <div className="content">
-     Here's the glimpse of the previous editions of Jagriti. Exciting events ranging from <br /> guest talks to competitions were successfully held,inundated with participants <br />from all over India.
-    </div>
-        <div className="allimg">  
-        <div className="img1">
-        <img className="img1size"src={Group2} alt="op" />
-      </div>
-      <div className="img2">
-        <img className="img2size" src={Group1} alt="op" />
-      </div>
-      </div>
-    
-      
-      </div>
-    </div>
-  
-
-
-  )
+  );
 }
 
-export default Throwback
+export default Throwback;
