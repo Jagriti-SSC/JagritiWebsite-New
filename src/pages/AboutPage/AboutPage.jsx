08f89ea58452f4f0193cec8aa0a5f5d4c2323972
mpatwa98 <mandeep.patwa.met21@itbhu.ac.in>
2023-03-10 17:49:42 +0530

    made about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
-import "./AboutPage.css"
+import React from "react";
+import "./AboutPage.css";
+import ClubCard from "../../components/card_aboutPage/ClubCard";
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <>
+      <div className="text-center my-10 mx-5">
+        <img className="mx-auto" src="/assets/Jagriti_big_logo.png" alt="" />
+        <h2 className="text-5xl text-blue font-Montserrat tracking-tight font-bold">
+          JAGRITI
+        </h2>
+        <p className="p-3 lg:text-xl w-[70vw] mx-auto tracking-normal text-[#0B1641] font-popins">
+          Jagriti is an enlightening and edifying annual social service fast
+          concentrated primarily on raising awareness through an assortment of
+          educative and profound thinking events. We conduct an array of events
+          circulating the theme of social issues. Jognitiis on eller to
+          self-contentment wherein we help the underprivileged, borrowing time
+          and drawing attention from our surplus lives.
+        </p>
+      </div>
+      <div className="text-center my-10 mx-5">
+        <img className="w-[300px] mx-auto" src="/assets/SSC_logo.png" alt="" />
+        <h2 className="text-blue font-Montserrat text-5xl tracking-tight font-bold">
+          SOCIAL SERVICE COUNCIL
+        </h2>
+        <p className="p-3 lg:text-xl w-[70vw] mx-auto tracking-normal text-[#0B1641] font-popins">
+          Social Service Council IIT(BHU) Varanasi embraces of the clube
+          dedicated to bringing about positive changes in our society's social
+          aspects. We are committed to developing and disseminating professional
+          knowledge, critical analysis and drafting innovative solutions,
+          consequently contributing to overall responsive social welfare. We
+          believe that the inherent humane quaities and the sprit of
+          volunteerism already existent in every individual needs to be nurtured
+          and further developed, thereby blooming out with the full potential to
+          benefit the society as a whole.
+        </p>
+      </div>
+      {/* <div className="grid gap-4 lg:grid-cols-2 lg:grid-rows-2 justify-center"> */}
+      <div className="lg:flex m-5 p-5 gap-5 justify-center">
+        <div className="my-5">
+          <ClubCard
+            clubLogo="SC_logo.png"
+            clubName="Sahyog Club"
+            clubDescription='Sahyog Club, with a mission of "improving quality education in government schools," collaborates with school teachers to improve the basic literacy and numeracy skills of children, leading to uplift their family conditions and background.'
+          />
+        </div>
+        <div className="my-5">
+          <ClubCard
+            clubLogo="KUC_logo.png"
+            clubName="Kashi Utkarsh Club"
+            clubDescription="Kashi Utkarsh, as the name suggests, is the development of Kashi. The development is a process that creates growth and positive change, and the volunteers of Kashi Utkarsh strive to get this change in bastis (slums). The prime motto of Kashi Utkarsh is to improve the quality of life of basti people by alleviating poverty through children's education."
+          />
+        </div>
+      </div>
+      <div className="lg:flex m-5 p-5 gap-5 justify-center">
+        <div className="my-5">
+          <ClubCard
+            clubLogo="HHC_logo.png"
+            clubName="Health and Hygiene Club"
+            clubDescription="The Health & Hygiene club promotes and advocates a safe and healthy environment for the society. Our various initiatives involve organising blood donation camps, medicine collection drive and spreading awareness on sensitive issues such as menstruation, diseases, healthy habits, sustainability, etc."
+          />
+        </div>
+        <div className="my-5">
+          <ClubCard
+            clubLogo="SPC_logo.png"
+            clubName="Social Projects' Club"
+            clubDescription="Social Projects' Club has taken the challenge of empowering society by employing simple and effective methods. We identify the public's problems and incorporate our technical skills with proper planning and strategy to come up with innovative solutions."
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
