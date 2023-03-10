b0d3983367f7dcddf0a30c5ed391f3c43708715c
mdathar <mdathar4403@gmail.com>
2023-03-10 21:43:59 +0530

    TeamPage_Btn

--- src/pages/TeamPage/GalleryData.js
+++ src/pages/TeamPage/GalleryData.js
@@ -1,73 +1,91 @@
 export const GalleryData = [
     {
         id:1,
-        title:"car",
-        name:"John",
+        title:"Convenors",
+        name:"John1",
         image:"./images/a1.jpg"
     },
     {
-        id:3,
-        title:"car",
+        id:2,
+        title:"General Secretary",
         name:"Riya",
         image:"./images/a2.jpg"
     },
     {
-        id:2,
-        title:"car",
+        id:3,
+        title:"Senior Advisor",
         name:"Sonu",
         image:"./images/a3.jpg"
     },
     {
         id:4,
-        title:"car2",
+        title:"Technology",
         name: "Rahul",
         image:"./images/a4.jpg"
     },
     {
         id:5,
-        title:"bike",
+        title:"UI/UX Design",
         name: "Rocky",
         image:"./images/b1.jpg"
     },
     {
-        id:7,
-        title:"bike",
+        id:6,
+        title:"Marketing",
         name: "Jack",
         image:"./images/b2.jpg"
     },
     {
         id:7,
-        title:"car2",
+        title:"Creative Design",
         name:"John",
         image:"./images/a1.jpg"
     },
     {
         id:8,
-        title:"car",
+        title:"Publicity",
         name:"Riya",
         image:"./images/a2.jpg"
     },
     {
         id:9,
-        title:"car2",
+        title:"Public Relations",
         name:"Sonu",
         image:"./images/a3.jpg"
     },
     {
         id:10,
-        title:"car2",
+        title:"Hospitality",
         name: "Rahul",
         image:"./images/a4.jpg"
     },
     {
         id:11,
-        title:"bike",
+        title:"Content",
         name: "Rocky",
         image:"./images/b1.jpg"
     },
     {
         id:12,
-        title:"bike",
+        title:"Security",
+        name: "Jack",
+        image:"./images/b2.jpg"
+    },
+    {
+        id:13,
+        title:"Decoration",
+        name: "Jack",
+        image:"./images/b2.jpg"
+    },
+    {
+        id:14,
+        title:"Social Media",
+        name: "Jack",
+        image:"./images/b2.jpg"
+    },
+    {
+        id:15,
+        title:"Club Secreatary",
         name: "Jack",
         image:"./images/b2.jpg"
     },
