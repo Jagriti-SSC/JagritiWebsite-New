2cd5e6e0a3c1fca146bf823b080d0e7cbb51e0db
Manu Shukla <shukla.manu09@gmail.com>
2023-03-11 21:32:33 +0530

    Minor Margin Changes

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -5,16 +5,18 @@ const array = [1,2,3,4,5,6,7,8,9]
 
 const Sponsor = () => {
     const [width,setWidth]=useState(0);
-    const carousel = useRef();
+    const carousel = useRef(null);
+    const innerCarousel = useRef(null);
+
     useEffect(()=>{
-        setWidth(carousel.current.scrollWidth-carousel.current.offsetWidth);
+        setWidth(carousel.current.scrollWidth-carousel.current.offsetWidth + 50);
         console.log(carousel.current.offsetWidth);
         console.log(carousel.current.scrollWidth);
-    },[])
+    },[innerCarousel.current])
 
     const variants = {
         animate:{
-            x: [0,-1035],
+            x: [0,-width],
             transition:{
                 x:{
                     repeat: Infinity,
@@ -27,15 +29,15 @@ const Sponsor = () => {
     }
 
   return (
-    <div className='flex flex-col gap-8 py-8 mb-7'>
+    <div className='flex flex-col gap-8 py-8 mb-10'>
     <div className='flex justify-center'>
-        <h3 className=' text-blue  font-Montserrat font-bold md:text-3xl sm:text-2xl text-xl lg:text-5xl'>
+        <h3 className=' text-blue  font-Montserrat font-bold' style={{fontSize: "40px"}}>
         SPONSORS
         </h3>
     </div>
     <div className=' mx-auto w-full'>
         <motion.div className='overflow-hidden' ref={carousel}>
-            <motion.div className='flex items-center gap-9 ' >
+            <motion.div className='flex items-center gap-9 ' ref={innerCarousel}>
                 {
                     array.map(item => 
                     <motion.div className='md:min-w-[15rem] sm:min-w-[6rem] min-w-[4rem] ml-5' variants={variants} animate="animate" >
