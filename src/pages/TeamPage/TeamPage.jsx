7faa0c8377a3aabed8ed7d2534017630b8baf6c4
Manu Shukla <shukla.manu09@gmail.com>
2023-03-11 13:38:23 +0530

    Added Draggable Filter Items

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,34 +1,39 @@
 import { GalleryData } from "./GalleryData";
-import { useEffect, useState } from "react";
-// import car from './images/'
-import './TeamPage.css'
+import { useEffect, useState, useRef } from "react";
+import "./TeamPage.css";
 import { motion, AnimatePresence } from "framer-motion";
-// import { Link } from "react-router-dom";
 import TeamCard from "../../components/TeamCard/TeamCard";
 
 function TeamPage() {
-
   const [data, setData] = useState([]);
   const [collection, setCollection] = useState([]);
-  const [active, setActive] = useState('Our Team');
-  const [activebtn, setActiveBtn] = useState('Our Team');
+  const [active, setActive] = useState("Our Team");
+  const [activebtn, setActiveBtn] = useState("Our Team");
+
+  const [width, setWidth] = useState(0);
+  const carousel = useRef(null);
+  const innerCarousel = useRef(null);
+
 
   useEffect(() => {
+    // console.log(carousel.current)
     setData(GalleryData);
-    setCollection([...new Set(GalleryData.map((item) => item.title))])
-  }, [])
+    setCollection([...new Set(GalleryData.map((item) => item.title))]);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth );
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth)
+  }, [innerCarousel.current]);
 
   const gallery_filter = (itemData) => {
     const filterData = GalleryData.filter((item) => item.title === itemData);
-    console.log(itemData)
+    // console.log(itemData);
     setData(filterData);
     setActive(itemData);
-    setActiveBtn('')
-  }
+    setActiveBtn("");
+  };
   function setColor() {
-    setData(GalleryData)
-    setActiveBtn('Our Team')
-    setActive("Our Team")
+    setData(GalleryData);
+    setActiveBtn("Our Team");
+    setActive("Our Team");
   }
 
   return (
@@ -38,42 +43,59 @@ function TeamPage() {
           <div className="our-team">
             <h1>{active}</h1>
           </div>
-          <ul>
-            <li>
-              <button onClick={setColor}
-                className={activebtn === 'Our Team' ? 'selected' : " "}>All</button>
-            </li>
-            {
-              collection.map((item) =>
-                <li>
-                  <button onClick={() => { gallery_filter(item) }}
-                    className={active === item ? 'selected' : " "}>{item}</button>
-                </li>)}
-          </ul>
-        </div>
-        <motion.div
-          layout
-          className="galleryContainer">
-          <AnimatePresence>
-            {
-              data.map((item) =>
-                <motion.div
-                  animate={{ opacity: 1, scale: 1 }}
-                  initial={{ opacity: 0, scale: 0 }}
-                  exit={{ opacity: 0, scale: 0 }}
-                  transition={{ duration: 0.36 }}
-                  layout
-                  key={item.id}
-                  className="galleryItem">
 
-                  <TeamCard name = {item.name} image={item.image} icon={item.icon} />
-                </motion.div>
-              )}
+          <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+            <motion.div
+              className="inner-carousel"
+              ref={innerCarousel}
+              drag="x"
+              dragConstraints={{
+                right: 0,
+                left: -width,
+              }}
+         
+            >
+              <button
+                onClick={setColor}
+                className={activebtn === "Our Team" ? "selected" : " "}
+              >
+                All
+              </button>
+
+              {collection.map((item, index) => (
+                <button
+                key={index}
+                  onClick={() => {
+                    gallery_filter(item);
+                  }}
+                  className={active === item ? "selected" : " "}
+                >
+                  {item}
+                </button>
+              ))}
+            </motion.div>
+          </motion.div>
+        </div>
+        <motion.div layout className="galleryContainer">
+          <AnimatePresence>
+            {data.map((item) => (
+              <motion.div
+                animate={{ opacity: 1, scale: 1 }}
+                initial={{ opacity: 0, scale: 0 }}
+                exit={{ opacity: 0, scale: 0 }}
+                transition={{ duration: 0.36 }}
+                layout
+                key={item.id}
+                className="galleryItem"
+              >
+                <TeamCard name={item.name} image={item.image} />
+              </motion.div>
+            ))}
           </AnimatePresence>
         </motion.div>
       </div>
-    </div >
+    </div>
   );
 }
 
-export default TeamPage
+export default TeamPage;
