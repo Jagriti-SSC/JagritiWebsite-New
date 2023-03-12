9ff0dd7e8072c97a376f08a5e5474f9646dde1f5
Aarjav Jain <aarjavjain2309@gmail.com>
2023-03-12 18:53:25 +0530

    color changes

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -6,7 +6,7 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import "./Timeline.css";
 import { FlagFill } from "@styled-icons/bootstrap/FlagFill";
-import {CheckCircleFill} from "@styled-icons/bootstrap/CheckCircleFill"
+import { CheckCircleFill } from "@styled-icons/bootstrap/CheckCircleFill";
 
 function Timeline() {
   return (
@@ -23,71 +23,81 @@ function Timeline() {
           contentArrowStyle={{
             borderRight: `7px solid #1A589B`,
           }}
-          iconStyle={{ background: "#1A589B", color: "#ffffff" }}
-          icon={<FlagFill style={{ padding:0 }}/>}
+          iconStyle={{
+            background: "white",
+            color: "#1A589B",
+            border: "1px solid #1A589B",
+          }}
+          icon={<FlagFill style={{ padding: 0 }} />}
         >
           <h3 className="title">Jagriti was formed</h3>
-
-          <p className="timeline-content">Jagriti was formed by the social service council</p>
+          <p className="timeline-content">
+            Jagriti was formed by the social service council
+          </p>
         </VerticalTimelineElement>
-        
+
         <VerticalTimelineElement
           className="box"
           contentStyle={{
-            background: "#D3D3D3",
+            border: "1px solid #1A589B",
             color: "#1A589B",
           }}
           contentArrowStyle={{
             borderRight: `7px solid #1A589B`,
           }}
-          iconStyle={{ background: "#1A589B", color: "#ffffff" }}
-          icon={<FlagFill style={{ padding:0 }}/>}
+          iconStyle={{
+            background: "#1A589B",
+            color: "white",
+          }}
+          icon={<FlagFill style={{ padding: 0 }} />}
         >
           <h3 className="title">Photography Pre-Event</h3>
-
-          <p className="timeline-content">Testing</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="box"
-          contentStyle={{
-            background: "#D3D3D3",
-            color: "#1A589B",
-          }}
-          contentArrowStyle={{
-            borderRight: `7px solid #1A589B`,
-          }}
-          iconStyle={{ background: "#1A589B", color: "#ffffff" }}
-          icon={<FlagFill style={{ padding:0 }} />}
-        >
-          <h3 className="title">Photography Pre-Event</h3>
-
           <p className="timeline-content">Testing</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="box"
           contentStyle={{
             background: "#1A589B",
+            // border: "1px solid #1A589B",
             color: "white",
           }}
           contentArrowStyle={{
             borderRight: `7px solid #1A589B`,
           }}
-          iconStyle={{ background: "#1A589B", color: "#ffffff" }}
-          icon={<FlagFill style={{ padding:0 }} />}
+          iconStyle={{
+            background: "white",
+            color: "#1A589B",
+            border: "1px solid #1A589B",
+          }}
+          icon={<FlagFill style={{ padding: 0 }} />}
         >
           <h3 className="title">Photography Pre-Event</h3>
-
+          <p className="timeline-content">Testing</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="box"
+          contentStyle={{
+            // background: "#1A589B",
+            border: "1px solid #1A589B",
+            color: "#1A589B",
+          }}
+          contentArrowStyle={{
+            borderRight: `7px solid #1A589B`,
+          }}
+          iconStyle={{
+            background: "#1A589B",
+            color: "white",
+          }}
+          icon={<FlagFill style={{ padding: 0 }} />}
+        >
+          <h3 className="title">Photography Pre-Event</h3>
           <p className="timeline-content">Testing</p>
         </VerticalTimelineElement>
 
-
         <VerticalTimelineElement
-        iconStyle={{ background: "#1A589B", color: "#ffffff" }}
-        icon={<CheckCircleFill style={{ padding:0 }}/>}
-        
+          iconStyle={{ background: "#1A589B", color: "#ffffff" }}
+          icon={<CheckCircleFill style={{ padding: 0 }} />}
         ></VerticalTimelineElement>
-        
-        
       </VerticalTimeline>
     </div>
   );
