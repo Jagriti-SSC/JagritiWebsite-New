04e20e4a9f712912de52d1062bebcba1b7a66859
Manu Shukla <shukla.manu09@gmail.com>
2023-03-13 00:39:41 +0530

    Fixed Sponsor Carousel

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -1,60 +1,26 @@
-import React,{useState,useEffect, useRef} from 'react'
-import { motion } from 'framer-motion'
-import Corousel from '../corousel/Corousel'
-const array = [1,2,3,4,5,6,7,8,9]
+import React from "react";
+import Marquee from "react-fast-marquee";
+import Corousel from "../corousel/Corousel";
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Sponsor = () => {
-    const [width,setWidth]=useState(0);
-    const carousel = useRef(null);
-    const innerCarousel = useRef(null);
-
-    useEffect(()=>{
-        setWidth(carousel.current.scrollWidth-carousel.current.offsetWidth + 50);
-        console.log(carousel.current.offsetWidth);
-        console.log(carousel.current.scrollWidth);
-    },[innerCarousel.current])
-
-    const variants = {
-        animate:{
-            x: [0,-1040],
-            transition:{
-                x:{
-                    repeat: Infinity,
-                    repeatType : "loop",
-                    duration: 10,
-                    ease: "linear",
-                }
-            },
-        },
-    }
-
   return (
-    <div className='flex flex-col gap-8 py-8 mb-10'>
-    <div className='flex justify-center'>
-        <h3 className=' text-blue  font-Montserrat font-bold' style={{fontSize: "40px"}}>
-        SPONSORS
+    <div className="flex flex-col gap-8 py-8 mb-10">
+      <div className="flex justify-center">
+        <h3
+          className=" text-blue  font-Montserrat font-bold"
+          style={{ fontSize: "40px" }}
+        >
+          SPONSORS
         </h3>
+      </div>
+      <Marquee style={{overflow:"hidden"}} gradient={false} speed="35" className=" mx-auto w-full">
+        {array.map((item, index) => (
+          <Corousel key={index}></Corousel>
+        ))}
+      </Marquee>
     </div>
-    <div className=' mx-auto w-full'>
-        <motion.div className='overflow-hidden' ref={carousel}>
-            <motion.div className='flex items-center gap-9 ' ref={innerCarousel}>
-                {
-                    array.map(item => 
-                    <motion.div className='md:min-w-[15rem] sm:min-w-[6rem] min-w-[4rem] ml-5' variants={variants} animate="animate" >
-                    <Corousel>
+  );
+};
 
-                    </Corousel>
-                    </motion.div>
-                   )
-                }
-            </motion.div>
-        </motion.div>
-    </div>
-   
-    </div>
-    
-    
-  )
-}
-
-export default Sponsor
+export default Sponsor;
