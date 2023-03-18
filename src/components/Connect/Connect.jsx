2c9090bda5f116cd49cd914de931ae312c44c435
Manu Shukla <shukla.manu09@gmail.com>
2023-03-19 02:31:37 +0530

    Minor text change

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -46,8 +46,8 @@ const Connect = () => {
 <div className="flex justify-between ss:max-w-[500px]  md:basis-4/5 xs:basis-3/4 basis-full rounded-lg bg-white items-stretch mx-auto py-2 px-[6px] max-w-full gap-1">
     <input  onChange={changeEmail}
         type="email"
-        value={email} required className=" text-black  pl-1 w-[70%] md:pl-8  outline-none border-none focus:ring-0 placeholder:md:text-start placeholder:text-center " placeholder="email address" />
-    <label onClick={saveEmail} className=" bg-black cursor-pointer shrink-0 text-white px-4 md:px-6 py-2 rounded-lg sm:text-sm md:text-lg ss:text-[16px] text-[14px] min-w-max">remind me</label>
+        value={email} required className=" text-black  pl-1 w-[70%] md:pl-8  outline-none border-none focus:ring-0 placeholder:md:text-start placeholder:text-center " placeholder="Email Address" />
+    <label onClick={saveEmail} className=" bg-black cursor-pointer shrink-0 text-white px-4 md:px-6 py-2 rounded-lg sm:text-sm md:text-lg ss:text-[16px] text-[14px] min-w-max">Remind Me</label>
 </div>
 
 
