5bca94c53e01068993d5c529f79be16db7dc0f26
Ishika Banerjee <120471767+ishika-tia@users.noreply.github.com>
2024-02-13 02:21:03 +0530

    Update galleryFirstLine.jsx

--- src/components/galleryFirstLine/galleryFirstLine.jsx
+++ src/components/galleryFirstLine/galleryFirstLine.jsx
@@ -4,26 +4,26 @@ import Marquee from "react-fast-marquee";
 import "./galleryFirstLine.css";
 
 
-import img1 from '../../assets/images/jagriti_img1.png';
-import img2 from '../../assets/images/jagriti_img2.png';
-import img3 from '../../assets/images/jagriti_img3.png';
-import img4 from '../../assets/images/jagriti_img4.png';
-import img5 from '../../assets/images/jagriti_img5.png';
-import img6 from '../../assets/images/jagriti_img6.png';
-import img7 from '../../assets/images/jagriti_img7.png';
-import img8 from '../../assets/images/jagriti_img8.png';
-import img9 from '../../assets/images/jagriti_img9.png';
-import img10 from '../../assets/images/jagriti_img10.png';
-import img11 from '../../assets/images/jagriti_img11.png';
-import img12 from '../../assets/images/jagriti_img12.png';
-import img13 from '../../assets/images/jagriti_img13.png';
-import img14 from '../../assets/images/jagriti_img14.png';
-import img15 from '../../assets/images/jagriti_img15.png';
-import img16 from '../../assets/images/jagriti_img16.png';
-import img17 from '../../assets/images/jagriti_img17.png';
-import img18 from '../../assets/images/jagriti_img18.png';
-import img19 from '../../assets/images/jagriti_img19.png';
-import img20 from '../../assets/images/jagriti_img20.png';
+import img1 from '../../assets/images/jagriti_img1.webp';
+import img2 from '../../assets/images/jagriti_img2.webp';
+import img3 from '../../assets/images/jagriti_img3.webp';
+import img4 from '../../assets/images/jagriti_img4.webp';
+import img5 from '../../assets/images/jagriti_img5.webp';
+import img6 from '../../assets/images/jagriti_img6.webp';
+import img7 from '../../assets/images/jagriti_img7.webp';
+import img8 from '../../assets/images/jagriti_img8.webp';
+import img9 from '../../assets/images/jagriti_img9.webp';
+import img10 from '../../assets/images/jagriti_img10.webp';
+import img11 from '../../assets/images/jagriti_img11.webp';
+import img12 from '../../assets/images/jagriti_img12.webp';
+import img13 from '../../assets/images/jagriti_img13.webp';
+import img14 from '../../assets/images/jagriti_img14.webp';
+import img15 from '../../assets/images/jagriti_img15.webp';
+import img16 from '../../assets/images/jagriti_img16.webp';
+import img17 from '../../assets/images/jagriti_img17.webp';
+import img18 from '../../assets/images/jagriti_img18.webp';
+import img19 from '../../assets/images/jagriti_img19.webp';
+import img20 from '../../assets/images/jagriti_img20.webp';
 import { Diversity } from "styled-icons/fluentui-system-filled";
 
 const GalleryFirstLine = () => {
@@ -34,8 +34,20 @@ const GalleryFirstLine = () => {
       </div >
       <Marquee style={{overflow:"hidden"}} gradient={false} speed="25" className="py-5 w-full mx-auto relative" >
         
-        <p className="cursor-pointer mx-10 ">Event abc</p>
-        <p className="cursor-pointer mx-10 ">Event abc</p>
+        <p className="cursor-pointer mx-10 ">One Frame One Cause</p>
+        <p className="cursor-pointer mx-10 ">Visual Catalysts</p>
+        <p className="cursor-pointer mx-10 ">AI Hackathon</p>
+        <p className="cursor-pointer mx-10 ">Spot The Green</p>
+        <p className="cursor-pointer mx-10 ">Social Entrepreneurship Hackathon</p>
+        <p className="cursor-pointer mx-10 ">Beautiful Impact</p>
+        <p className="cursor-pointer mx-10 ">Page Painting</p>
+        <p className="cursor-pointer mx-10 ">Eye Checkup</p>
+        <p className="cursor-pointer mx-10 ">Policy Challenge</p>
+        <p className="cursor-pointer mx-10 ">Global Debate</p>
+        <p className="cursor-pointer mx-10 ">NGO Activity</p>
+        <p className="cursor-pointer mx-10 ">Kulhad/Gullak Painting</p>
+        <p className="cursor-pointer mx-10 ">Leadership Symposium</p>
+        <p className="cursor-pointer mx-10 ">Self Defence Workshop</p>
 
       </Marquee>
       <Marquee style={{overflow:"hidden"}} direction="right" gradient={false} speed="35" className="py-5 w-full mx-auto relative" >
