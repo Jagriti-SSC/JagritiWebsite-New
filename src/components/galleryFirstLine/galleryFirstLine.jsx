d6d4007b4c73d7154aaf2d8df84b2c10863c287e
varnit-saini-iitbhu <varnitsaini1112@gmail.com>
2025-12-15 14:03:41 +0530

    added imgaes

--- src/components/galleryFirstLine/galleryFirstLine.jsx
+++ src/components/galleryFirstLine/galleryFirstLine.jsx
@@ -24,6 +24,16 @@ import img17 from '../../assets/images/jagriti_img17.webp';
 import img18 from '../../assets/images/jagriti_img18.webp';
 import img19 from '../../assets/images/jagriti_img19.webp';
 import img20 from '../../assets/images/jagriti_img20.webp';
+import img21 from '../../assets/images/jagriti_img21.webp';
+import img22 from '../../assets/images/jagriti_img22.webp';
+import img23 from '../../assets/images/jagriti_img23.webp';
+import img24 from '../../assets/images/jagriti_img24.webp';
+import img25 from '../../assets/images/jagriti_img25.webp';
+import img26 from '../../assets/images/jagriti_img26.webp';
+import img27 from '../../assets/images/jagriti_img27.webp';
+import img28 from '../../assets/images/jagriti_img28.webp';
+import img29 from '../../assets/images/jagriti_img29.webp';
+import img30 from '../../assets/images/jagriti_img30.webp';
 import { Diversity } from "styled-icons/fluentui-system-filled";
 
 const GalleryFirstLine = () => {
@@ -52,45 +62,48 @@ const GalleryFirstLine = () => {
       </Marquee>
       <Marquee style={{overflow:"hidden"}} direction="right" gradient={false} speed="35" className="py-5 w-full mx-auto relative" >
         <img src={img1} className=" mx-[10px] box-shadow h-40 " alt="Jagriti Image"></img>
-        <img src={img2} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img9} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img3} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img4} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img14} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img10} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img5} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img6} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img16} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img17} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img14} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img21} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
 
       </Marquee>
       <Marquee style={{overflow:"hidden"}} gradient={false} speed="45" className="py-5 mx-auto w-full relative" >
         <img src={img7} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img28} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img23} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img22} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img24} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img29} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img26} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img30} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img8} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img13} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img19} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img20} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img11} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img1} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img2} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
 
       </Marquee>
       <Marquee style={{overflow:"hidden"}} gradient={false} direction="right" speed="55" className="py-5 mx-auto w-full relative" >
         <img src={img12} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img13} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img14} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img15} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img27} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img25} className="h-40  mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img16} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img17} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img6} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img22} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img7} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img15} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
       </Marquee>
       <Marquee style={{overflow:"hidden"}} gradient={false} speed="65" className="py-5 mx-auto w-full relative" >
         <img src={img3} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img5} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img14} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
-        <img src={img9} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img23} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img6} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img11} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img19} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img20} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img2} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
+        <img src={img21} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
         <img src={img18} className="h-40 mx-[10px] box-shadow" alt="Jagriti Image"></img>
       </Marquee>
     </div>
